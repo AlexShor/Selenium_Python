b9cb1e0508bbc6dc35e85382af38d2ca3d78d011
Alexander Shorokhov <lex.shor@gmail.com>
2023-06-19 22:37:25 +0500

    Update file

--- section2/lesson4_step8.py
+++ section2/lesson4_step8.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import math
-import os
 
 
 def calc(x):
