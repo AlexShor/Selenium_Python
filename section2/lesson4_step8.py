c3b8e8722a3803f90ffa18e18c01419dcbf69f59
Alexander Shorokhov <lex.shor@gmail.com>
2023-06-19 22:59:08 +0500

    Update file

--- section2/lesson4_step8.py
+++ section2/lesson4_step8.py
@@ -5,17 +5,27 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import math
-
-
-def calc(x):
-    return str(math.log(abs(12 * math.sin(int(x)))))
-
+import pyperclip
 
 link = "http://suninjuly.github.io/explicit_wait2.html"
 prise = 100
 
 browser = webdriver.Chrome()  # Инициализируем драйвер браузера. Открытие окна браузера
 browser.implicitly_wait(5)
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+def get_number_alert():  # Получение числа из алерта
+    alert = browser.switch_to.alert
+    alert_text = alert.text
+    number = alert_text[alert_text.index(':') + 2:]
+    pyperclip.copy(number)
+    print(number)
+    alert.accept()
+
 
 try:
     browser.get(link)
@@ -28,11 +38,7 @@ try:
     browser.find_element(By.ID, 'answer').send_keys(y)
     browser.find_element(By.CSS_SELECTOR, "button.btn[id='solve']").click()
 
-    # Получение числа из алерта
-    alert = browser.switch_to.alert
-    alert_text = alert.text
-    print(alert_text[alert_text.index(':') + 2:])
-    alert.accept()
+    get_number_alert()
 
     # assert "successful" in message.text
 
@@ -40,7 +46,6 @@ try:
 #     print('\033[91m' + str(e)[:str(e).index('\n')])
 
 finally:
-    time.sleep(5)
     browser.quit()  # После выполнения всех действий мы должны не забыть закрыть окно браузера
 
 # не забываем оставить пустую строку в конце файла
