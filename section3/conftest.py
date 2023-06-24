ddc57689a0041b9cd6f85ea3b509062065930e90
Alexander Shorokhov <lex.shor@gmail.com>
2023-06-24 19:16:05 +0500

    Update conftest.py

--- section3/conftest.py
+++ section3/conftest.py
@@ -4,26 +4,27 @@ from selenium import webdriver
 from dotenv import load_dotenv
 
 
-@pytest.fixture(scope="class")
+# @pytest.fixture(scope="module")
+def links():
+    lessons = ['236895', '236896', '236897', '236898', '236899', '236903', '236904', '236905']
+    # lessons = ['236895', '236896']
+    list_links = [f"https://stepik.org/lesson/{lesson_id}/step/1" for lesson_id in lessons]
+    return list_links
+
+
+@pytest.fixture(scope="function")
 def browser():
-    # print("\nstart browser for test..")
     options = webdriver.ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     browser = webdriver.Chrome(options=options)
-    browser.implicitly_wait(15)
+    browser.implicitly_wait(20)
     yield browser
-    # print("\nquit browser..")
     browser.quit()
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture()
 def credentials():
     load_dotenv()
     EMAIL = os.getenv('EMAIL')
     PASSWORD = os.getenv('PASSWORD')
     return {'email': EMAIL, 'password': PASSWORD}
-
-
-@pytest.fixture(scope="class")
-def link():
-    return "https://stepik.org/lesson/236895/step/1"
